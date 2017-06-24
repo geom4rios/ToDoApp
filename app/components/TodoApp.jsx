7d38a5624cd6714befe0419b740c7026b092f649
geom4rios <geom4rios@gmail.com>
2017-06-24 20:50:42 +0300

    rendering todoList

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,26 @@
+var React = require('react');
+var TodoList = require('TodoList');
+
+var TodoApp = React.createClass({
+    getInitialState: function () {
+      return {
+          todos: [
+              {id: 1, text: 'walk the dog'},
+              {id: 2, text: 'clean the yard'},
+              {id: 3, text: 'Leave mail on porch'},
+              {id: 4, text: 'Play videogames'}
+          ]
+      }
+    },
+    render: function () {
+        var {todos} = this.state;
+
+        return (
+            <div>
+                <TodoList todos={todos} />
+            </div>
+        );
+    }
+});
+
+module.exports = TodoApp;
