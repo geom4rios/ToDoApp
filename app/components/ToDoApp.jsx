e5f91149e72e96485ee941a15ff06c5425543525
geom4rios <geom4rios@gmail.com>
2017-06-24 19:35:24 +0300

    Add ToDoApp component

---
+++ app/components/ToDoApp.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+
+var ToDoApp = React.createClass({
+    render: function () {
+        return (
+            <div>
+                Main, just main.
+            </div>
+        );
+    }
+});
+
+module.exports = ToDoApp;
